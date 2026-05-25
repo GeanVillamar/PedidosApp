a0896f9d14e6ea26680d3202b1bb60f74bd65f8b
Gean Villamar <geanfranklinvillamar@gmail.com>
2026-05-24 21:27:08 -0500

    Test Github Action comment

--- PedidosApp/Program.cs
+++ PedidosApp/Program.cs
@@ -7,6 +7,8 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+//github actions comments test
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
